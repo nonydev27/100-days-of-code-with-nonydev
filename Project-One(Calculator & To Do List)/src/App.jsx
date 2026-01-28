1eb1e62272c6df49c8176b7d90683aa45b65bb76
Karl Djansi <karldjansi123@gmail.com>
2026-01-28 16:23:04 +0000

    Update App.jsx

--- Project-One(Calculator & To Do List)/src/App.jsx
+++ Project-One(Calculator & To Do List)/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <div className='container'>
           <div className=' container container-fluid header p-10 m-6 bg-black flex justify-center'>
             <h1 className='text-white text-3xl'>100 DAYS OF CODE WITH NONYDEV</h1>
-
+            <
           </div>
         </div>
     </>
