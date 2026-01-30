1ad78d5aae3bc4c5b11bd53821f439dbc97067ba
nonydev <karldjansi123@gmail.com>
2026-01-30 04:13:50 +0000

    added other repos

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -10,6 +10,15 @@ function App() {
             <h1 className='text-white text-3xl'>100 DAYS OF CODE WITH NONYDEV</h1>
 
           </div>
+
+
+          <div className='info-header'> 
+            <h3>Project: Calculator</h3> 
+            <h5>Duration: 10 days</h5>
+            <div className='w-48 h-1 bg-green-500'></div>
+          </div>
+
+
         </div>
     </>
   )
