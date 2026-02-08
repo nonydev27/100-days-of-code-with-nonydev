886a513d60b4a29aa3bc695bf9fe124d843ff077
nonydev <karldjansi123@gmail.com>
2026-02-08 21:07:54 +0000

    ...

--- color_flipper/app/app.tsx
+++ color_flipper/app/app.tsx
@@ -2,7 +2,7 @@
 export default function app() {
   return (
     
-    <div className="flex flex-1 w-full">
+    <div className="flex justify-center">
       <div className="">
         <button className="bg-red-400">red</button>
        <button className="bg-blue-400">blue</button>
