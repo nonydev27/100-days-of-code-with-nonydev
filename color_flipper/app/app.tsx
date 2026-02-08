04baa8f6e72315334f4fb31515e0e3a61eebbb8b
nonydev <karldjansi123@gmail.com>
2026-02-08 21:17:28 +0000

    ...

--- color_flipper/app/app.tsx
+++ color_flipper/app/app.tsx
@@ -2,7 +2,7 @@
 export default function app() {
   return (
     
-    <div className="flex justify-center">
+    <div className="font-sans flex items-center justify-center min-h-screen py-2">
       <div className="space-x-2 ">
         <button className="bg-red-400 p-4 uppercase">red</button>
        <button className="bg-blue-400 p-4 uppercase">blue</button>
