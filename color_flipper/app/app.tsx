d040c3c204f874581f6513d08ecb3994e7b87df7
nonydev <karldjansi123@gmail.com>
2026-02-08 21:47:45 +0000

    ...

--- color_flipper/app/app.tsx
+++ color_flipper/app/app.tsx
@@ -1,12 +1,14 @@
-import {useState} from "react";
+"use client";
 
-const [backgroundColor, setBackgroundColor] = useState("white");
+// import {useState} from "react";
+
+// const [backgroundColor, setBackgroundColor] = useState("white");
 export default function app() {
   return (
     
     <div className="font-sans flex items-center justify-center min-h-screen py-2">
       <div className="space-x-2 ">
-        <button className="bg-red-400 p-4 uppercase text-white font-bold">red</button>
+        <button className="bg-red-400 p-4 uppercase text-white font-bold hover:bg-red-600">red</button>
        <button className="bg-blue-400 p-4 uppercase text-white font-bold">blue</button>
        <button className="bg-green-400 p-4 uppercase text-white font-bold">green</button>
        <button className="bg-gray-400 p-4 uppercase text-white font-bold">random</button>
