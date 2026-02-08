2d7ed829733300493144f2b129b81a0dfc87e7fa
nonydev <karldjansi123@gmail.com>
2026-02-08 21:48:08 +0000

    ...

--- color_flipper/app/app.tsx
+++ color_flipper/app/app.tsx
@@ -9,9 +9,9 @@ export default function app() {
     <div className="font-sans flex items-center justify-center min-h-screen py-2">
       <div className="space-x-2 ">
         <button className="bg-red-400 p-4 uppercase text-white font-bold hover:bg-red-600">red</button>
-       <button className="bg-blue-400 p-4 uppercase text-white font-bold">blue</button>
-       <button className="bg-green-400 p-4 uppercase text-white font-bold">green</button>
-       <button className="bg-gray-400 p-4 uppercase text-white font-bold">random</button>
+       <button className="bg-blue-400 p-4 uppercase text-white font-bold hover:bg-red-600">blue</button>
+       <button className="bg-green-400 p-4 uppercase text-white font-bold hover:bg-red-600">green</button>
+       <button className="bg-gray-400 p-4 uppercase text-white font-bold hover:bg-red-600">random</button>
       </div>
     </div>
 
