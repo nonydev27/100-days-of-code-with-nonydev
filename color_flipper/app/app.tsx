5a80b440f20e5821e0c5ede85ed38bcc27f81c65
nonydev <karldjansi123@gmail.com>
2026-02-08 21:12:20 +0000

    ...

--- color_flipper/app/app.tsx
+++ color_flipper/app/app.tsx
@@ -3,11 +3,11 @@ export default function app() {
   return (
     
     <div className="flex justify-center">
-      <div className="space-x-2">
-        <button className="bg-red-400">red</button>
-       <button className="bg-blue-400">blue</button>
-       <button className="bg-green-400">green</button>
-       <button className="bg-gray-400">random</button>
+      <div className="space-x-2 ">
+        <button className="bg-red-400 p-4 uppercase">red</button>
+       <button className="bg-blue-400 p-4 uppercase">blue</button>
+       <button className="bg-green-400 p-4 uppercase">green</button>
+       <button className="bg-gray-400 p-4 uppercase">random</button>
       </div>
     </div>
 
